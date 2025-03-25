b6437000027544fd02d36dd51be7779d7a43ae5c
wyr0o <brykin-2006@mail.ru>
2025-03-25 10:08:19 +0300

    Add files via upload

---
+++ вебинар 6 задание 3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+long long fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    int num = 0;
+    cout << "Введите число: ";
+    cin >> num;
+
+    cout << "Числа Фибоначчи : ";
+    for (int i = 0; i < num; ++i) {
+        cout << fibonacci(i) << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
