6f85e05b06674daa08708582dbd1b80314bd56df
wyr0o <brykin-2006@mail.ru>
2025-03-25 09:46:20 +0300

    Add files via upload

---
+++ вебинар 6 задание 2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void exponentiation(int value, int power)
+{
+	int result = 1;
+	for (int i = 0; i < power; i++)
+	{
+		result *= value;
+	}
+	cout << value << " в степени " << power << " = " << result<< endl;
+	
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	exponentiation(5, 2);
+	exponentiation(3, 3);
+	exponentiation(4, 4);
+}
