64e95491b52d6a496445add1826d15f2af7df6c5
wyr0o <brykin-2006@mail.ru>
2025-03-25 10:42:23 +0300

    Add files via upload

---
+++ вебинар 6 задание 3 сложное.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+long long fibonacciopt(int n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    long long a = 0;
+    long long b = 1;
+    long long temp =0;
+
+    for (int i = 2; i <= n; ++i) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+
+    int num;
+    cout << "Введите число: ";
+    cin >> num;
+
+    cout << "Числа Фибоначчи (opt): ";
+    for (int i = 0; i <= num; ++i) {
+        cout << fibonacciopt(i) << " ";
+    }
+    cout << std::endl;
+
+    return 0;
+}
